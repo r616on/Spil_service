80dabc6050078cf46d20a10f5db8795a98445118
r616on <r616on@gmail.com>
2021-10-22 22:36:54 +0300

    22_10

--- src/components/app.js
+++ src/components/app.js
@@ -1,52 +1,52 @@
-const row = document.querySelector(".main__conteiner");
-const numbers = [
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-  "ten",
-  "eleven",
-  "twelve",
-  "thirteen",
-  "fourteen",
-  "fifteen",
-  "sixteen",
-  "seventeen",
-  "eighteen",
-  "nineteen",
-];
+// const row = document.querySelector(".main__conteiner");
+// const numbers = [
+//   "one",
+//   "two",
+//   "three",
+//   "four",
+//   "five",
+//   "six",
+//   "seven",
+//   "eight",
+//   "nine",
+//   "ten",
+//   "eleven",
+//   "twelve",
+//   "thirteen",
+//   "fourteen",
+//   "fifteen",
+//   "sixteen",
+//   "seventeen",
+//   "eighteen",
+//   "nineteen",
+// ];
 
-numbers.forEach((elem, index) => {
-  const cart = document.createElement("div");
-  cart.className = "cart";
-  cart.innerHTML = `<div class="cart__title">Наименование заказа ${
-    index + 1
-  }</div>
-            <div class="cart__text">
-              Et harum quidem rerum facilis est et expedita distinctio, nam
-              libero tempore, cum soluta nobis est eligendi optio, cumque nihil
-              impedit, quo minus id, quod maxime placeat, facere possimus, omnis
-              voluptas assumenda est, omnis dolor repellendus! Duis aute irure
-              dolor in reprehenderit in voluptate, quis nostrum exercitationem
-              ullam corporis suscipit
-            </div>
-            <div class="cart__field first">
-              <div class="first__text">Звание</div>
-              <div class="first__value">12</div>
-            </div>
-            <div class="cart__field date">
-              <div class="date__text">Дедлайн</div>
-              <div class="date__value">03.11.2021</div>
-            </div>
-            <div class="cart__field price">
-              <div class="price__text">Сумма</div>
-              <div class="price__value">4500</div>
-            </div>`;
-  cart.classList.add(elem);
-  row.append(cart);
-});
+// numbers.forEach((elem, index) => {
+//   const cart = document.createElement("div");
+//   cart.className = "cart";
+//   cart.innerHTML = `<div class="cart__title">Наименование заказа ${
+//     index + 1
+//   }</div>
+//             <div class="cart__text">
+//               Et harum quidem rerum facilis est et expedita distinctio, nam
+//               libero tempore, cum soluta nobis est eligendi optio, cumque nihil
+//               impedit, quo minus id, quod maxime placeat, facere possimus, omnis
+//               voluptas assumenda est, omnis dolor repellendus! Duis aute irure
+//               dolor in reprehenderit in voluptate, quis nostrum exercitationem
+//               ullam corporis suscipit
+//             </div>
+//             <div class="cart__field first">
+//               <div class="first__text">Звание</div>
+//               <div class="first__value">12</div>
+//             </div>
+//             <div class="cart__field date">
+//               <div class="date__text">Дедлайн</div>
+//               <div class="date__value">03.11.2021</div>
+//             </div>
+//             <div class="cart__field price">
+//               <div class="price__text">Сумма</div>
+//               <div class="price__value">4500</div>
+//             </div>`;
+//   cart.classList.add(elem);
+//   row.append(cart);
+// });
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "./css/nulStyle.css";
 import "./css/style.css";
 import "./css/fonts.css";
 import "./scss/main.scss";
-import "./css/header.css";
-import "./css/footer.css";
+import "./scss/header.scss";
+import "./scss/footer.scss";
+import "./scss/calculator.scss";
 // import "./components/templated/templated.css";
-
 import "./components/app";
