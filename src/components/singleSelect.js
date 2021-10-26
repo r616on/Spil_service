39e4fb940e69befa3cba3db801a1a7d3ddab0fba
r616on <r616on@gmail.com>
2021-10-27 00:43:38 +0300

    calculate_vue_done

---
+++ src/components/singleSelect.js
@@ -0,0 +1,6 @@
+// Reset title
+// const reset = document.querySelector(".reset");
+// reset.addEventListener("click", () => {
+//   selectSingle_title.textContent =
+//     selectSingle_title.getAttribute("data-default");
+// });
